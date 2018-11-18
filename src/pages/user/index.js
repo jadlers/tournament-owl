2c651c71f716fe7cee5423f2fe4de406fa85bb3e
Jacob Adlers <jacob.adlers@gmail.com>
2018-11-18 03:49:43 +0100

    Add a simple friend list
    Don't know how I would make sure the names of your friends are up to
    date since I'm not using references to get the names every render. That
    would quickly eat on my free reads

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { Redirect } from '@reach/router';
+import { Link } from '@reach/router';
 import { Error } from '../../styled/components';
 
 import { getCurrentUser } from '../../Authentication/firebaseAuth';
 import { db } from '../../firebase';
 
 class User extends React.Component {
-  state = { fetching: true, user: null };
+  constructor(props) {
+    super(props);
+    this.state = { fetching: true, user: null };
+    this.fetchUser(props.uid);
+  }
 
-  async componentDidMount() {
-    const userRef = db.collection('users').doc(this.props.uid);
+  componentDidUpdate(prevProps) {
+    const { uid } = this.props;
+    if (uid !== prevProps.uid) this.fetchUser(uid);
+  }
+
+  fetchUser = async uid => {
+    console.log('fetching user');
+    const userRef = db.collection('users').doc(uid);
     const userDoc = await userRef.get();
     if (userDoc.exists) {
       this.setState({ user: userDoc.data(), fetching: false });
     } else {
       this.setState({ fetching: false });
     }
-  }
+  };
 
   render() {
     const { fetching, user } = this.state;
@@ -29,6 +40,15 @@ class User extends React.Component {
       <div>
         <h1>Here's the profile</h1>
         {user.username && <h2>{`Username: ${user.username}`}</h2>}
+        {user.friends && (
+          <ul>
+            {Object.keys(user.friends).map(friend => (
+              <li key={friend}>
+                <Link to={`../${user.friends[friend]}`}>{friend}</Link>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     );
   }
